ebe14120615ca386bea06d0a8930e506fc700414
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-05-18 11:40:13 +0700

    Add files via upload

--- Figure S7.R
+++ Figure S7.R
@@ -1,54 +1,26 @@
-<<<<<<< HEAD
 library(bvpSolve)
 library(ReacTran)
 library(deSolve)
-N <- 500
-v <- 0.5
-k <- 0.1/(1/v)
-D <- 0.4 # Assigning the parameters values
-r <- 0.42
-lamb <- (r*D)/(v^2) 
-alpha <- 6.13*(10^8)
-M0 <-  3.33*10^9
-xm <- 1
-times <- seq(0, 1, len=25)
-xgrid <- setup.grid.1D (x.up = 0, x.down = L, N = N)
-
-func <- function(t, state, parameters) {
-  with(as.list(c(state, parameters)),{
-    
-    dF <- -(r/alpha)*(B+M)*Food/(k+Food)+ advection.1D(C=Food, v=v,cx=xgrid)$dC  + tran.1D(C = Food, D = D, flux.up = v*F_in, flux.down = NULL,  dx = xgrid)$dC
-    dB <- r*(M*Food)/(k+Food) + advection.1D(C=B, v=v,cx=xgrid)$dC  + tran.1D(C = B, D = D, dx = xgrid)$dC
-    dM <- r*(M*Food)/(k+Food) + advection.1D(C=M, v=v,cx=xgrid)$dC  + tran.1D(C = M, D = D, dx = xgrid)$dC
-    
-    list(c(dF, dB, dM))})
-}
-
-parameters <- list(N = N, v=v , k=k, D=D, r=r, lamb=lamb, alpha=alpha, M0=M0)
-=======
-library(bvpSolve)
-library(ReacTran)
-library(deSolve)
-L <- 6
-N <- 211
-v <- 0.5
-k <- 0.1
-F_in <- 1/v
+L     <- 6
+N     <- 211
+v     <- 0.5
+k     <- 0.1
+F_in  <- 1/v
 kappa <- 0.1/(1/v)
-D <- 0.4             # Assigning the parameters values
-r <- 0.42
-lamb <- (r*D)/(v^2) 
+D     <- 0.4             # Assigning the parameters values
+r     <- 0.42
+lamb  <- (r*D)/(v^2) 
 alpha <- 6.13*(10^8)
 
 
 ## Solve for initial condition (i.e the stationary state without mutant)
 
 
-phi0     <- 0 # our object Numerical solution is phi
+phi0  <- 0    # our object Numerical solution is phi
               # phi0 is initial guess
               # v is phi's 1st derivative
 
-s <- seq(0,xic,length.out = N) # independent variable
+s     <- seq(0,xic,length.out = N) # independent variable
 
 func <- function(s, Y, pars){
   with (as.list(Y), {
@@ -64,15 +36,12 @@ bound <- function(i, Y, pars) {
     if(i==2) return (v)
   })
 }
-sguess = seq(0, xic, length.out = N)
+sguess <-  seq(0, xic, length.out = N)
 yguess <- matrix(ncol = N,
                  data = (rep(c(phi0,phi0-1), N)))
 rownames(yguess) <- c("phi", "v")
-
-
-
 Sol <-  bvpcol(x = s, func = func, bound = bound,
-               xguess = sguess, yguess = yguess, # Solving
+               xguess = sguess, yguess = yguess,    # Solving
                leftbc = 1)
 Sol[,2] <- Sol[,2]
 Sol[,1] <- Sol[,1]*D/v
@@ -83,42 +52,42 @@ vec_F_in  <- rep(1,N)
 Food_star <- Sol[,2]*F_in                 ### The initial condition of PDE system
 B_star    <- alpha*(vec_F_in - Food_star) ### for B and F
 xm        <- 3                            ### the first location mutant appear
-M0        <- 3.33                         ### mutant concentration at this location (xm)
+M0        <- B_star/2                     ### mutant concentration at this location (xm)
 
-times <- seq(0, 1,len=200)
-xgrid <- setup.grid.1D (x.up = 0, x.down = L, N = N) ## generating the gird for our solution
-x     <- seq(0,6,len=N)                              ### Our discretization points
-F_ini <- Food_star
-B_ini <- B_star                                      ### Our initial condition of the system
-M_ini <- rep(1,N)
-Mini <- function (x){
-  a <- abs(x-xm)
-  if (a<=(0.01/2))    ### Initial condition for Mutant
-  {
-    return(M0)
-  }
-  else    {
-    return(0)
-  }
-  
-}
+times     <- seq(0, 1,len=100)
+xgrid     <- setup.grid.1D (x.up = 0, x.down = L, N = N) ## generating the gird for our solution
+x         <- seq(0,6,len=N)                              ### Our discretization points
+F_ini     <- Food_star
+B_ini     <- B_star                                      ### Our initial condition of the system
+M_ini     <- rep(1,N)
+
+
+Mini      <- function (x){
+        a <- abs(x-xm)
+        if (a<=(0.01/2)){
+          return(M0)
+        }    ### Initial condition for Mutant
+        else{
+          return(0)
+        }
+        }
 for(i in 1:N){
   M_ini[i] <- Mini(x[i]) ### Asigning the value of function "Mini" for initial condition of Mutant
 }
 
-yini <- c(F_ini, B_ini, M_ini)
+yini <- c(F_ini, B_ini, M_ini) ## Initial condition to use in ode1D
 
 
 ### Solving the system using ode.1D
 
 Sol_system <- function(t, Y, parms) {
   
-    Food  <- Y[1:N]
-    B     <- Y[(N+1):(2*N)]
-    M     <- Y[((2*N)+1):(3*N)]
-    dFood <- -(r/alpha)*(B+M)*Food/(k+Food) + tran.1D(C = Food, D = D, flux.up = v*F_in, flux.down = NULL, v=v, dx = xgrid)$dC
-    dB    <- r*(B*Food)/(k+Food)            + tran.1D(C = B, D = D, flux.up = 0, flux.down = NULL, v=v, dx = xgrid)$dC   ###tran1D to describe divection diffusion equation
-    dM    <- r*(M*Food)/(k+Food)            + tran.1D(C = M, D = D, flux.up = 0, flux.down = NULL, v=v, dx = xgrid)$dC
+     Food  <- Y[1:N]
+     B     <- Y[(N+1):(2*N)]
+     M     <- Y[((2*N)+1):(3*N)]
+     dFood <- -(r/alpha)*(B+M)*Food/(k+Food) + tran.1D(C = Food, D = D, flux.up = v*F_in, flux.down = NULL, v=v, dx = xgrid)$dC
+     dB    <- r*(B*Food)/(k+Food)            + tran.1D(C = B, D = D, flux.up = 0, flux.down = NULL, v=v, dx = xgrid)$dC   ###tran1D to describe divection diffusion equation
+     dM    <- r*(M*Food)/(k+Food)            + tran.1D(C = M, D = D, flux.up = 0, flux.down = NULL, v=v, dx = xgrid)$dC
     
     return(list(c(dFood, dB, dM)))
 }
@@ -127,16 +96,15 @@ print(system.time(
   out  <- ode.1D(y = yini, func = Sol_system, times = times,nspec = 3, names = c("Food","B","M"), parms = NULL , dimens = N)
 ))
 
-a1   <- xm*rep(1,length(times)) +  sqrt(2*D*times) ## Different lines in the heatmap (from the article)
-a2   <- xm*rep(1,length(times)) -  sqrt(2*D*times)
-a3   <- xm*rep(1,length(times)) +  v*times
+a1     <- xm*rep(1,length(times)) +  sqrt(2*D*times) ## Different lines in the heatmap (from the article)
+a2     <- xm*rep(1,length(times)) -  sqrt(2*D*times)
+a3     <- xm*rep(1,length(times)) +  v*times
 
 ### Visualization
 
 par(mfrow = c(2,2))
 par(mar = c(3.7,3.7,4,4)+0.4)
-image(out,legend=TRUE,xlab="times",ylab="position", grid = xgrid$x.mid,which = "M")
+image(out,legend=TRUE, xlab="times", ylab="position", grid = xgrid$x.mid,which = "B")
 lines(times, a1, col="white")
 lines(times, a2, col="white")
 lines(times, a3, col="black")
->>>>>>> 680aa58c89f2ad5805f2661d0b2743ebe790d042
