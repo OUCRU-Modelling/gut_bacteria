001d7697addef87846e3449957360684f98a104e
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-05-18 20:46:10 +0700

    modified Figure S7

--- Figure S7.R
+++ Figure S7.R
@@ -6,22 +6,19 @@ N     <- 211
 v     <- 0.5
 k     <- 0.1
 F_in  <- 1/v
-kappa <- 0.1/(1/v)
-D     <- 0.4             # Assigning the parameters values
+kappa <- 0.1/(1/v)  # Note that we only use kappa to solve the initial condition for the with mutant system 
+D     <- 0.4        # Assigning the parameters values
 r     <- 0.42
 lamb  <- (r*D)/(v^2) 
 alpha <- 6.13*(10^8)
 
-
-## Solve for initial condition (i.e the stationary state without mutant)
-
-
+## Solve for initial condition (i.e the stationary state without mutant, F_star and B_star)
+## Basically, I replicate everything I did to drew the Figure S1 in Appendix.
 phi0  <- 0    # our object Numerical solution is phi
               # phi0 is initial guess
               # v is phi's 1st derivative
 
 s     <- seq(0,xic,length.out = N) # independent variable
-
 func <- function(s, Y, pars){
   with (as.list(Y), {
     dphi=v
@@ -29,7 +26,6 @@ func <- function(s, Y, pars){
     return(list(c(dphi, dv)))
   })
 }
-
 bound <- function(i, Y, pars) {
   with (as.list(Y), {
     if(i==1) return (phi-v-1) # boundary conditions
@@ -41,55 +37,47 @@ yguess <- matrix(ncol = N,
                  data = (rep(c(phi0,phi0-1), N)))
 rownames(yguess) <- c("phi", "v")
 Sol <-  bvpcol(x = s, func = func, bound = bound,
-               xguess = sguess, yguess = yguess,    # Solving
+               xguess = sguess, yguess = yguess,    # Solving non-mutant system
                leftbc = 1)
 Sol[,2] <- Sol[,2]
 Sol[,1] <- Sol[,1]*D/v
 
 #### Solve the system with mutant first appear at xm
 
-vec_F_in  <- rep(1,N)
-Food_star <- Sol[,2]*F_in                 ### The initial condition of PDE system
-B_star    <- alpha*(vec_F_in - Food_star) ### for B and F
+vec_F_in  <- rep(1,N)*F_in                ### because 
+Food_star <- Sol[,2]*F_in                 ### The initial condition of PDE system, is in Food whereas in without mutant, the solution is F/F_in
+B_star    <- alpha*(vec_F_in - Food_star) ### The equation of B_star and F_star in no mutant system (my previous reports)
 xm        <- 3                            ### the first location mutant appear
-M0        <- B_star/2                     ### mutant concentration at this location (xm)
-
-times     <- seq(0, 1,len=100)
+M0        <- 3.33*10^-11                  ### mutant concentration at this location (xm), initial concentration of Mutant
+times     <- seq(0, 1,len=100)            ### discretization of times
 xgrid     <- setup.grid.1D (x.up = 0, x.down = L, N = N) ## generating the gird for our solution
 x         <- seq(0,6,len=N)                              ### Our discretization points
 F_ini     <- Food_star
-B_ini     <- B_star                                      ### Our initial condition of the system
+B_ini     <- B_star                                      ### Our initial condition of the system (at t = 0)
 M_ini     <- rep(1,N)
-
-
 Mini      <- function (x){
         a <- abs(x-xm)
         if (a<=(0.01/2)){
           return(M0)
-        }    ### Initial condition for Mutant
+        }                                 ### Initial condition for Mutant
         else{
           return(0)
         }
         }
 for(i in 1:N){
-  M_ini[i] <- Mini(x[i]) ### Asigning the value of function "Mini" for initial condition of Mutant
+  M_ini[i] <- Mini(x[i])       ### Asigning the value of function "Mini" for initial condition of Mutant
 }
+yini <- c(F_ini, B_ini, M_ini) ### Initial condition to use in ode1D
 
-yini <- c(F_ini, B_ini, M_ini) ## Initial condition to use in ode1D
-
-
-### Solving the system using ode.1D
-
+### Solving the full system using ode.1D
 Sol_system <- function(t, Y, parms) {
-  
      Food  <- Y[1:N]
      B     <- Y[(N+1):(2*N)]
      M     <- Y[((2*N)+1):(3*N)]
      dFood <- -(r/alpha)*(B+M)*Food/(k+Food) + tran.1D(C = Food, D = D, flux.up = v*F_in, flux.down = NULL, v=v, dx = xgrid)$dC
-     dB    <- r*(B*Food)/(k+Food)            + tran.1D(C = B   , D = D, flux.up = 0     , flux.down = NULL, v=v, dx = xgrid)$dC   ###tran1D to describe divection diffusion equation
+     dB    <- r*(B*Food)/(k+Food)            + tran.1D(C = B   , D = D, flux.up = 0     , flux.down = NULL, v=v, dx = xgrid)$dC   ### tran1D to describe divection diffusion equation
      dM    <- r*(M*Food)/(k+Food)            + tran.1D(C = M   , D = D, flux.up = 0     , flux.down = NULL, v=v, dx = xgrid)$dC
-    
-    return(list(c(dFood, dB, dM)))
+     return(list(c(dFood, dB, dM)))
 }
 
 print(system.time(
@@ -101,10 +89,9 @@ a2     <- xm*rep(1,length(times)) -  sqrt(2*D*times)
 a3     <- xm*rep(1,length(times)) +  v*times
 
 ### Visualization
-
 par(mfrow = c(2,2))
 par(mar = c(3.7,3.7,4,4)+0.4)
-image(out,legend=TRUE, xlab="times", ylab="position", grid = xgrid$x.mid, which = "B")
+image(out,legend=TRUE, xlab="times", ylab="position", grid = xgrid$x.mid, which = "B") ### drawing heatmap
 lines(times, a1, col="white")
 lines(times, a2, col="white")
 lines(times, a3, col="black")
