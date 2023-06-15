54337afe6b465a50e509f3dff5b8555452bd2151
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-06-15 14:23:36 +0700

    changes

--- Figure 1C of Labavic [1].R
+++ Figure 1C of Labavic [1].R
@@ -6,20 +6,17 @@ L               <- 6
 k               <- 0.1
 r               <- 0.42
 alpha           <- 6.13*(10^8)
+tmax            <- 500  
+N               <- 6000
 ### it reached the stationary state
 v               <- seq(0, 2, len=30)       ### initialize array contains velocities
 D               <- seq(0,50, len=30)       ### initialize array contains Diffusion Coefficients
-lamb            <- matrix(rep(1, len=900),ncol=length(v),nrow=length(D))
 conc_profile    <- matrix(rep(1, len=900),ncol=length(v),nrow=length(D))  ### z-values in Heat map
 F_in            <- matrix(rep(1, len=900),ncol=length(v),nrow=length(D))
 i               <- 1
 j               <- 1
 while (i <= length(v)){
 while (j <= length(D)){
-lamb[i, j]      <- (r*D[j])/(v[i]^2)
-xic             <- (L*v[i])/D[j]
-tmax            <- 500  
-N               <- 10031                     ### N must be very large in order to abtain the exact solution compared to stationary system (the old code)
 F_in[i, j]      <- 1/v[i]
 vec_F_in        <- rep(1,N)*F_in[i, j]               
 times           <- seq(0, tmax,len=100)                        ### discretization of times
@@ -30,13 +27,13 @@ B_ini           <- 0.1*alpha*vec_F_in
 Sol_system      <- function(t, Y, parms) {
           Food  <- Y[1:N]
           B     <- Y[(N+1):(2*N)]
-          dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D, flux.up = 1     , flux.down = NULL, v=v, dx = xgrid)$dC
-          dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D, flux.up = 0     , flux.down = NULL, v=v, dx = xgrid)$dC
+          dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[j], flux.up = 1     , flux.down = NULL, v=v[i], dx = xgrid)$dC
+          dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[j], flux.up = 0     , flux.down = NULL, v=v[i], dx = xgrid)$dC
           return(list(c(dFood, dB)))
 }
 yini <- c(F_ini, B_ini)                                                                           
 
-out  <- ode.1D(y = yini, func = Sol_system, times = times,nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
+out  <- ode.1D(y = yini, func = Sol_system, times = times, nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
 
 conc_profile[i, j]  <- (out[tmax,2]-out[tmax,N+1])/F_in[i,j]
 j <- j+1
