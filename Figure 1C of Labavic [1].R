e90df11a343728d84764bdf153d7c98bbc318897
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-06-17 18:44:54 +0700

    latest version

--- Figure 1C of Labavic [1].R
+++ Figure 1C of Labavic [1].R
@@ -3,47 +3,49 @@ library(bvpSolve)
 library(ReacTran)
 library(deSolve)
 library(ggplot2)
-library(tidyr)
 library(tibble)
-library(dplyr)
 library(reshape2)
+library(viridis)
 library(fields)
-L         <- 6                                               ### Assigning the parameters values
-r         <- 0.42
-alpha     <- 6.13*(10^8)
-k         <- 0.1
-v         <- c(seq(0.05, 1, len=40),  seq(1.1, 5, len=10))     ### v and D scale have diffrent step sizes in it (Focus on Figure C, you can see the different leaps for diffrent interal of values)
-F_in      <- 1/v
-D         <- c(seq(0.05, 10, len=40), seq(11, 50, len=10))        
-tmax      <- 500                                             ### I choose tmax=500 because in the article, they say when our system pass t=500hrs it reached the stationary state
-N         <- 6000                                            ### N must be very large in order to abtain the exact solution compared to stationary system, but I still can put it 6000 because the consumption time problem
-i         <- 1
-j         <- 1
+L             <- 6                                               ### Assigning the parameters values
+r             <- 0.42
+alpha         <- 6.13*(10^8)
+k             <- 0.1
+v             <- c(seq(0.05, 0.1, len=19), seq(0.105, 0.2, len=15), seq(0.201, 0.5, len = 14), seq(0.505, 1, len=8), seq(1, 2.6, len =5))     ### v and D scale have diffrent step sizes in it (Focus on Figure C, you can see the different leaps for diffrent interval of values)
+F_in          <- 1/v
+D             <- c(seq(0.01, 0.10, len=10), seq(0.11, 0.50, len=20), seq(0.501, 1, len=10), seq(1.01, 5, len =10), seq(5.01, 10, len =4), seq(11, 50, len =10))        
+tmax          <- 500                                             ### I choose tmax=500 because in the article, they say when our system pass t=500hrs it reached the stationary state
+N             <- 6000                                            ### N must be very large in order to abtain the exact solution compared to stationary system, but I still can put it 6000 because the consumption time problem
+i             <- 1
+
 conc_profile  <- matrix(rep(1, len = length(v)*length(D)), ncol=length(v), nrow=length(D))
 while(i<=length(D)){                                         ### creating the loop to draw the heatmap
   for(j in 1:length(v)){
     vec_F_in  <- rep(1,N)*F_in[j]               
-    times     <- seq(0, tmax,len=100)                        ### discretization of times
+    times     <- seq(0, tmax,len=70)                         ### discretization of times
     xgrid     <- setup.grid.1D (x.up = 0, x.down = L, N = N) ### generating the gird for our solution
     x         <- xgrid$x.int                                 ### Our discretization points
     F_ini     <- vec_F_in*0.9
     B_ini     <- 0.1*alpha*vec_F_in
     Sol_system <- function(t, Y, parms) {
-      Food  <- Y[1:N]
-      B     <- Y[(N+1):(2*N)]
-      dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[i], flux.up = 1   , flux.down = NULL, v=v[j], dx = xgrid)$dC
-      dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[i], flux.up = 0   , flux.down = NULL, v=v[j], dx = xgrid)$dC
+      Food    <- Y[1:N]
+      B       <- Y[(N+1):(2*N)]
+      dFood   <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[i], flux.up = 1 , flux.down = NULL, v=v[j], dx = xgrid)$dC
+      dB      <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[i], flux.up = 0 , flux.down = NULL, v=v[j], dx = xgrid)$dC
       return(list(c(dFood, dB)))
     }
-    yini <- c(F_ini, B_ini)                                                                           
+       yini   <- c(F_ini, B_ini)                                                                           
     print(system.time(
-      out  <- ode.1D(y = yini, func = Sol_system, times = times, nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
+      out     <- ode.1D(y = yini, func = Sol_system, times = times, nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
     ))
-    #j <- j+1
+  
     conc_profile[i,j] <- (out[length(times),2]-out[length(times),N+1])/F_in[j]
   }
   i <- i+1
 }
+par(mar = c(6, 5, 5, 7) + 0.05 )
+image.plot(t(conc_profile),col = NULL, axes=FALSE, legend = TRUE)
+contour(t(conc_profile), add=TRUE)
 
-image.plot(t(conc_profile), x=v, y=D, ylim=c(0.05, 15), xlim=c(0.05, 1.4)) ### two ways to draw heatmap, but this image.plot using 'fields' package is more visualize
-#heatmap(conc_profile, Rowv=NA, Colv=NA, col = heat.colors(256))
+
+#heatmap(conc_profile, Rowv=NA, Colv=NA, col = terrain.colors(256))
--- Figure S1-Appendix of [1].R
+++ Figure S1-Appendix of [1].R
@@ -3,20 +3,16 @@ library(bvpSolve)
 library(ReacTran)
 library(deSolve)
 L         <- 6
-v         <- seq(0.05, 2, len=5 )
+v         <- 3
 k         <- 0.1
 F_in      <- 1/v
-D         <- seq(0.05, 30,len=5)        # Assigning the parameters values
+D         <- 0.0051       # Assigning the parameters values
 r         <- 0.42
 alpha     <- 6.13*(10^8)
 tmax      <- 500        ### I choose tmax=500 because in the article, they say when our system pass t=500hrs
                         ### it reached the stationary state
 N         <- 6000       ### N must be very large in order to abtain the exact solution compared to stationary system (the old code)
-i <- 1
-j <- 1
-while(i<=length(v)){
-while(j<=length(D)){
-vec_F_in  <- rep(1,N)*F_in[i]               
+vec_F_in  <- rep(1,N)*F_in               
 times     <- seq(0, tmax,len=100)                        ### discretization of times
 xgrid     <- setup.grid.1D (x.up = 0, x.down = L, N = N) ### generating the gird for our solution
 x         <- xgrid$x.int                                 ### Our discretization points
@@ -25,19 +21,16 @@ B_ini     <- 0.1*alpha*vec_F_in
 Sol_system <- function(t, Y, parms) {
   Food  <- Y[1:N]
   B     <- Y[(N+1):(2*N)]
-  dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[j], flux.up = 1     , flux.down = NULL, v=v[i], dx = xgrid)$dC
-  dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[j], flux.up = 0     , flux.down = NULL, v=v[i], dx = xgrid)$dC
+  dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D, flux.up = 1     , flux.down = NULL, v=v, dx = xgrid)$dC
+  dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D, flux.up = 0     , flux.down = NULL, v=v, dx = xgrid)$dC
   return(list(c(dFood, dB)))
 }
 yini <- c(F_ini, B_ini)                                                                           
 print(system.time(
   out  <- ode.1D(y = yini, func = Sol_system, times = times, nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
 ))
-j <- j+1
-}
-  i <- i+1
-}
-#out[,2:(N+1)]   <- out[,2:(N+1)]/F_in
-#outtime <- seq(from = tmax-60, to = tmax, by = 20)
-#matplot.1D(out, which = "Food", ylim = c(0, 1), las = 1, xlim = c(0,6), subset = time %in% outtime, grid = xgrid$x.mid , xlab="x", ylab='Food', main = "Solution 2", type='l', lwd = 2, col= 'red') ### plot in 2D each of Food; Bacteria or Mutant
+
+out[,2:(N+1)]   <- out[,2:(N+1)]/F_in
+outtime <- seq(from = tmax-60, to = tmax, by = 20)
+matplot.1D(out, which = "Food", ylim = c(0, 1), las = 1, xlim = c(0,6), subset = time %in% outtime, grid = xgrid$x.mid , xlab="x", ylab='Food', main = "Solution 2", type='l', lwd = 2, col= 'red') ### plot in 2D each of Food; Bacteria or Mutant
 
