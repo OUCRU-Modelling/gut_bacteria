e17b6af24e493c537837931189f5f32c2da39f5c
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-06-17 13:07:48 +0700

    latest update

--- Figure 1C of Labavic [1].R
+++ Figure 1C of Labavic [1].R
@@ -1,23 +1,28 @@
-rm(list = ls())
+#rm(list = ls())
 library(bvpSolve)
 library(ReacTran)
 library(deSolve)
-L         <- 6
-v         <- seq(0.05, 10, len=30)
-k         <- 0.1
-F_in      <- 1/v
-D         <- seq(0.05, 50, len=30)        # Assigning the parameters values
+library(ggplot2)
+library(tidyr)
+library(tibble)
+library(dplyr)
+library(reshape2)
+library(fields)
+L         <- 6                                               ### Assigning the parameters values
 r         <- 0.42
 alpha     <- 6.13*(10^8)
-tmax      <- 500        ### I choose tmax=500 because in the article, they say when our system pass t=500hrs
-### it reached the stationary state
-N         <- 6000       ### N must be very large in order to abtain the exact solution compared to stationary system (the old code)
-i <- 1
-j <- 1
-conc_profile    <- matrix(rep(1, len = length(v)*length(D)), ncol=length(v), nrow=length(D))
-while(i<=length(v)){
-  for(j in 1:length(D)){
-    vec_F_in  <- rep(1,N)*F_in[i]               
+k         <- 0.1
+v         <- c(seq(0.05, 1, len=40),  seq(1.1, 5, len=10))     ### v and D scale have diffrent step sizes in it (Focus on Figure C, you can see the different leaps for diffrent interal of values)
+F_in      <- 1/v
+D         <- c(seq(0.05, 10, len=40), seq(11, 50, len=10))        
+tmax      <- 500                                             ### I choose tmax=500 because in the article, they say when our system pass t=500hrs it reached the stationary state
+N         <- 6000                                            ### N must be very large in order to abtain the exact solution compared to stationary system, but I still can put it 6000 because the consumption time problem
+i         <- 1
+j         <- 1
+conc_profile  <- matrix(rep(1, len = length(v)*length(D)), ncol=length(v), nrow=length(D))
+while(i<=length(D)){                                         ### creating the loop to draw the heatmap
+  for(j in 1:length(v)){
+    vec_F_in  <- rep(1,N)*F_in[j]               
     times     <- seq(0, tmax,len=100)                        ### discretization of times
     xgrid     <- setup.grid.1D (x.up = 0, x.down = L, N = N) ### generating the gird for our solution
     x         <- xgrid$x.int                                 ### Our discretization points
@@ -26,8 +31,8 @@ while(i<=length(v)){
     Sol_system <- function(t, Y, parms) {
       Food  <- Y[1:N]
       B     <- Y[(N+1):(2*N)]
-      dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[j], flux.up = 1   , flux.down = NULL, v=v[i], dx = xgrid)$dC
-      dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[j], flux.up = 0   , flux.down = NULL, v=v[i], dx = xgrid)$dC
+      dFood <- -(r/alpha)*B*Food/(k+Food) + tran.1D(C = Food, D = D[i], flux.up = 1   , flux.down = NULL, v=v[j], dx = xgrid)$dC
+      dB    <- r*B*Food/(k+Food)          + tran.1D(C = B   , D = D[i], flux.up = 0   , flux.down = NULL, v=v[j], dx = xgrid)$dC
       return(list(c(dFood, dB)))
     }
     yini <- c(F_ini, B_ini)                                                                           
@@ -35,8 +40,10 @@ while(i<=length(v)){
       out  <- ode.1D(y = yini, func = Sol_system, times = times, nspec = 2, names = c("Food","B"), parms = NULL , dimens = N)
     ))
     #j <- j+1
-    conc_profile[i,j] <- (out[length(times),2]-out[length(times),N+1])/F_in[i]
+    conc_profile[i,j] <- (out[length(times),2]-out[length(times),N+1])/F_in[j]
   }
-    i <- i+1
+  i <- i+1
 }
-heatmap(t(conc_profile), Rowv=NA, Colv=NA, col = hcl.colors(61))
+
+image.plot(t(conc_profile), x=v, y=D, ylim=c(0.05, 15), xlim=c(0.05, 1.4)) ### two ways to draw heatmap, but this image.plot using 'fields' package is more visualize
+#heatmap(conc_profile, Rowv=NA, Colv=NA, col = heat.colors(256))
