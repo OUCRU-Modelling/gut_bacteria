0ac03e5d8a68e938ef6003ed475589c00c58186a
LeNgocHoangHuan <131734211+LeNgocHoangHuan@users.noreply.github.com>
2023-06-23 05:56:23 +0700

    testing with different values of D and v

--- Thesis 1B-washout limit.R
+++ Thesis 1B-washout limit.R
@@ -2,13 +2,13 @@ rm(list = ls())
 library(bvpSolve)
 library(ReacTran)
 library(deSolve)
-L           <- 61
+L           <- 6
 N           <- 10031                 ### Spatial discretization points, must be odd in order to obtain the exact stationary solution
-v           <- c(1,0.5,0.5)          ### flow velocity of Food, Antibiotic, Bacteria and Mutant respectively
-D           <- c(05.0,0.2,0.2)       ### Diffusion coefficient of Food, Antibiotic, Bacteria and Mutant respectively
+v           <- c(1, 0.3857,  0.5)    ### flow velocity of Food, Antibiotic, Bacteria 
+D           <- c(05.0, 0.2, 2)       ### Diffusion coefficient of Food, Antibiotic, Bacteria respectively
 k           <- 0.1                   ### Monod constant
-F_in        <- 1/0.5                 ### Food concentration at the entrance of the gut
-A_in        <- 1/0.5                 ### Antibiotic concentration at the entrance of the gut
+F_in        <- 1/v[1]                ### Food concentration at the entrance of the gut
+A_in        <- 1/v[2]                ### Antibiotic concentration at the entrance of the gut
 
 ##### Assigning the parameters values #####
 r           <- 0.42                  ### Growth rate of Bacteria 
@@ -18,8 +18,8 @@ A_50        <- 0.1                   ### Concentration of Antibiotic correspondi
 tmax        <- 700
 alpha1      <- (r*F_in)/(k+F_in)
 alpha2      <- (A_in)/(A_50 + A_in)
-delta_wo    <- (alpha1 - (v[1]^2)/(4*D[1]))*(1/alpha2)        ### Possible wash out limit
-delta_max   <- delta_wo + 2*10^-1
+delta_wo    <- (alpha1 - (v[3]^2)/(4*D[3]))*(1/alpha2)        ### Possible wash out limit
+delta_max   <- delta_wo + 0.01
 times       <- seq(0, tmax,len=200)                           ### discretization of times
 xgrid       <- setup.grid.1D (x.up = 0, x.down = L, N = N)    ### generating the gird for our solution
 x           <- xgrid$x.mid                                    ### We should cho x.mid rather than x.int 
@@ -32,8 +32,8 @@ Sol_system  <- function(t, Y, parms) {
   A         <- Y[(N+1):(2*N)]
   B         <- Y[((2*N)+1):(3*N)]
   dFood     <- -(r/alpha)*B*Food/(k+Food)       + tran.1D(C = Food, D = D[1] , flux.up = 1   , flux.down = NULL, v = v[1], dx = xgrid)$dC
-  dA        <- -(delta_max/beta)*B*(A)/(A+A_50) + tran.1D(C = A   , D = D[1] , flux.up = 1   , flux.down = NULL, v = v[1], dx = xgrid)$dC
-  dB        <- r*B*Food/(k+Food) + -delta_max*B*(A)/(A+A_50) + tran.1D(C = B , D = D[1], flux.up = 0 , flux.down = NULL, v = v[1], dx = xgrid)$dC                                         ### tran1D to describe divection diffusion equation
+  dA        <- -(delta_max/beta)*B*(A)/(A+A_50) + tran.1D(C = A   , D = D[2] , flux.up = 1   , flux.down = NULL, v = v[2], dx = xgrid)$dC
+  dB        <- r*B*Food/(k+Food) + -delta_max*B*(A)/(A+A_50) + tran.1D(C = B , D = D[3], flux.up = 0 , flux.down = NULL, v = v[3], dx = xgrid)$dC                                         ### tran1D to describe divection diffusion equation
   
   return(list(c(dFood, dA, dB)))
 }
